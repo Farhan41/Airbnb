75fa8a4ea9841375ee4fba48c8e507f97442abe7
Farhan Ferdous <74263768+Farhan41@users.noreply.github.com>
2023-12-20 18:30:22 +0600

    Auth UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from 'next'
 import { Inter, Nunito } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/navbar/Navbar'
+import ClientOnly from './components/ClientOnly'
+import Modal from './components/modals/Modal'
+import RegisterModal from './components/modals/RegisterModal'
+import ToastProvider from './providers/ToastProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +22,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>
+       <ClientOnly>
+        <ToastProvider/>
+       <RegisterModal/>
+       <Navbar/>
+       {/* <Modal actionLabel='submit' title='Hello World' isOpen /> */}
+       </ClientOnly>
         {children}
       </body>
     </html>
