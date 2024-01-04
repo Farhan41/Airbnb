4aa6a58a31e96ae56be5080ec54b1874cfb5003c
Farhan Ferdous <74263768+Farhan41@users.noreply.github.com>
2024-01-04 17:28:09 +0600

    next-6

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,21 +1,24 @@
-'use client'
-import Image from "next/image"
+'use client';
 
-interface AvatarProps{
+import Image from "next/image";
+
+interface AvatarProps {
   src: string | null | undefined;
 }
 
-
-const Avatar: React.FC<AvatarProps> = ({src}) => {
-  return (
+const Avatar: React.FC<AvatarProps> = ({ src }) => {
+  return ( 
     <Image 
-    className="rounded-full" 
-    height="30" 
-    width="30" 
-    alt="avatar" 
-    src={src || "/images/placeholder.jpg"}
+      className="rounded-full" 
+      height="30" 
+      width="30" 
+      alt="Avatar" 
+      src={src || '/images/placeholder.jpg'}
     />
-  )
+   );
 }
+ 
+export default Avatar;
 
-export default Avatar
+
+//some changes needed
